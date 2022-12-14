9f7098b0ac30cb6895f8c3d4f615ea8548770ae3
sophie-nguyen-0 <113055711+sophie-nguyen-0@users.noreply.github.com>
2022-12-14 14:38:23 -0500

    Update main.cs

--- main.cs
+++ main.cs
@@ -7,25 +7,25 @@ class Program
     public static async Task Main()
     {
         string answer;
-	    int counter = 0;
+	int counter = 0;
 
-		Console.Write("do you want to quit? (yes or no): ");
+	Console.Write("do you want to quit? (yes or no): ");
         answer = Convert.ToString(Console.ReadLine());
 
-		while (answer == "no")
-		{
-				counter ++;
-			    Console.WriteLine("the counter is at " + counter);
+	while (answer == "no")
+	{
+		counter ++;
+		Console.WriteLine("the counter is at " + counter);
 
-			    await File.WriteAllTextAsync("WriteText.txt", counter.ToString());
-			    Console.WriteLine("File Created ...");
+		await File.WriteAllTextAsync("WriteText.txt", counter.ToString());
+		Console.WriteLine("File Created ...");
 
-        	    var someText = await File.ReadAllTextAsync(@"WriteText.txt");
+        	 var someText = await File.ReadAllTextAsync(@"WriteText.txt");
 
-		        Console.Write("do you want to quit? (yes or no): ");
-	        	answer = Convert.ToString(Console.ReadLine());
-		}
-		Console.WriteLine("\nDone.");
+		 Console.Write("do you want to quit? (yes or no): ");
+	         answer = Convert.ToString(Console.ReadLine());
+	}
+	Console.WriteLine("\nDone.");
 
 
     }
