2947168a42240d0cfa890deb5b3bb90b6647fd17
sophie-nguyen-0 <113055711+sophie-nguyen-0@users.noreply.github.com>
2022-12-14 02:09:43 +0000

    cs again

--- main.cs
+++ main.cs
@@ -18,18 +18,18 @@ class Program
 			{
 				counter ++;
 				Console.WriteLine("the counter is at " + counter);
-	
+
 				await File.WriteAllTextAsync("WriteText.txt", counter.ToString());
 	    		Console.WriteLine("File Created ...");
-	
+
 	        	var someText = await File.ReadAllTextAsync(@"WriteText.txt");
-	
+
 				Console.Write("do you want to quit? (yes or no): ");
 	        	answer = Convert.ToString(Console.ReadLine());
 			}
 		}
-			counter = 0;
-		  Console.WriteLine("\nDone.");
+		counter = 0;
+		Console.WriteLine("\nDone.");
 
 
     }
