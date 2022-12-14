18865d440dfc3f73dcc7dc8409c4c60f21ef615b
sophie-nguyen-0 <113055711+sophie-nguyen-0@users.noreply.github.com>
2022-12-14 02:17:53 +0000

    cs

--- main.cs
+++ main.cs
@@ -4,32 +4,32 @@ using System.IO;
 
 class Program
 {
-  public static async Task Main()
+  	public static async Task Main()
 	{
       	String answer;
-	    int counter = 0;
+	      int counter = 0;
 
-		Console.Write("do you want to quit? (yes or no): ");
+		    Console.Write("do you want to quit? (yes or no): ");
         answer = Convert.ToString(Console.ReadLine());
 
-		while (answer == "no")
-		{
-			if (answer == "no")
-			{
-				counter ++;
-				Console.WriteLine("the counter is at " + counter);
+		    while (answer == "no")
+		    {
+			      if (answer == "no")
+			      {
+				        counter ++;
+				        Console.WriteLine("the counter is at " + counter);
 
-				await File.WriteAllTextAsync("WriteText.txt", counter.ToString());
-	    		Console.WriteLine("File Created ...");
+				        await File.WriteAllTextAsync("WriteText.txt", counter.ToString());
+	    		      Console.WriteLine("File Created ...");
 
-	        	var someText = await File.ReadAllTextAsync(@"WriteText.txt");
+	        	    var someText = await File.ReadAllTextAsync(@"WriteText.txt");
 
-				Console.Write("do you want to quit? (yes or no): ");
-	        	answer = Convert.ToString(Console.ReadLine());
-			}
-		}
-		counter = 0;
-		Console.WriteLine("\nDone.");
+				        Console.Write("do you want to quit? (yes or no): ");
+	        	    answer = Convert.ToString(Console.ReadLine());
+			      }
+		    }
+		    counter = 0;
+		    Console.WriteLine("\nDone.");
 
 
     }
