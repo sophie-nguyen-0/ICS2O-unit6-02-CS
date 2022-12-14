b42b3ae0d2265c5bacdf8f57d760c562b78be36e
sophie-nguyen-0 <113055711+sophie-nguyen-0@users.noreply.github.com>
2022-12-14 19:48:59 +0000

    another spacing fix

--- main.cs
+++ main.cs
@@ -4,27 +4,27 @@ using System.IO;
 
 class Program
 {
-    public static async Task Main()
+public static async Task Main()
     {
         string answer;
-    	int counter = 0;
-    
-    	Console.Write("do you want to quit? (yes or no): ");
+        int counter = 0;
+        
+        Console.Write("do you want to quit? (yes or no): ");
         answer = Convert.ToString(Console.ReadLine());
-    
-    	while (answer == "no")
-    	{
-    		counter ++;
-    		Console.WriteLine("the counter is at " + counter);
-    
-    		await File.WriteAllTextAsync("WriteText.txt", counter.ToString());
-    		Console.WriteLine("File Created ...");
-    
+        
+        while (answer == "no")
+        {
+            counter ++;
+            Console.WriteLine("the counter is at " + counter);
+            
+            await File.WriteAllTextAsync("WriteText.txt", counter.ToString());
+            Console.WriteLine("File Created ...");
+            
             var someText = await File.ReadAllTextAsync(@"WriteText.txt");
-    
-    		Console.Write("do you want to quit? (yes or no): ");
-    	    answer = Convert.ToString(Console.ReadLine());
-    	}
-    	Console.WriteLine("\nDone.");
+            
+            Console.Write("do you want to quit? (yes or no): ");
+            answer = Convert.ToString(Console.ReadLine());
+        }
+        Console.WriteLine("\nDone.");
     }
 }
