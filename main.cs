d0b7de96c039f4c5220c0f88d337eecad4f7c30e
sophie-nguyen-0 <113055711+sophie-nguyen-0@users.noreply.github.com>
2022-12-14 23:36:34 +0000

    spaces

--- main.cs
+++ main.cs
@@ -8,20 +8,20 @@ class Program
     {
         string answer;
         int counter = 0;
-        
+
         Console.Write("do you want to quit? (yes or no): ");
         answer = Convert.ToString(Console.ReadLine());
-        
+
         while (answer == "no")
         {
             counter ++;
             Console.WriteLine("the counter is at " + counter);
-            
+
             await File.WriteAllTextAsync("WriteText.txt", counter.ToString());
             Console.WriteLine("File Created ...");
-            
+
             var someText = await File.ReadAllTextAsync(@"WriteText.txt");
-            
+
             Console.Write("do you want to quit? (yes or no): ");
             answer = Convert.ToString(Console.ReadLine());
         }
